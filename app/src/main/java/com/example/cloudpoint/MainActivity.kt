ec78f0f9a7ef9e0f44630f42faf58de8c64ef971
OxVidhi <81790585+Bhawna1203@users.noreply.github.com>
2022-05-24 10:49:49 +0530

    added permission for location

--- app/src/main/java/com/example/cloudpoint/MainActivity.kt
+++ app/src/main/java/com/example/cloudpoint/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.cloudpoint
 
 import android.app.Dialog
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import android.os.AsyncTask
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +16,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.example.cloudpoint.databinding.ActivityMainBinding
 import java.io.BufferedReader
 import java.io.IOException
@@ -29,87 +34,35 @@ class MainActivity : AppCompatActivity() {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
 
-        }
+            if(!isLocationEnabled()){
+                Toast.makeText(
+                    this,
+                    "Your location provider is turned off. PLease turn is on",
+                    Toast.LENGTH_SHORT
+                ).show()
 
+                //If you directly want to come in the example when screen starts with the page
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }else{
+                Toast.makeText(
+                    this,
+                    "Your location provider is already turned on",
+                    Toast.LENGTH_SHORT
+                ).show()
 
-        private inner class CallAPILoginAsyncTask(): AsyncTask<Any, Void, String>(){
-
-            private lateinit var customProgressDialog: Dialog
-
-            override fun onPreExecute() {
-                super.onPreExecute()
-                showProgressDialog()
-                CallAPILoginAsyncTask().execute()
-            }
-            override fun doInBackground(vararg p0: Any?): String {
-                var result: String
-                var connection: HttpURLConnection ?= null
-
-                try{
-                    val url = URL("https://run.mocky.io/v3/223c5dfc-9639-48d2-b37f-2ce1dbe077b3")
-                    connection = url.openConnection() as HttpURLConnection
-                    connection.doInput = true
-                    connection.doOutput = true
-
-                    val httpsResult : Int = connection.responseCode
-                    if(httpsResult == HttpsURLConnection.HTTP_OK){
-                        val inputStream = connection.inputStream
-                        val reader = BufferedReader(
-                            InputStreamReader(inputStream)
-                        )
-                        val stringBuilder = StringBuilder()
-                        var line : String?
-                        try{
-                            while(reader.readLine().also{line = it} != null){
-                                stringBuilder.append(line + "\n")
-                            }
-                        }catch (e: IOException){
-                            e.printStackTrace()
-                        }finally {
-                            try{
-                                inputStream.close()
-                            }catch (e:IOException){
-                                e.printStackTrace()
-                            }
-                        }
-                        result = stringBuilder.toString()
-                    }else{
-                        result = connection.responseMessage
-                    }
-                }catch (e: SocketTimeoutException){
-                    result = "Connection timeout"
-                }catch(e:Exception){
-                    result = "Error : " + e.message
-                }finally {
-                    connection?.disconnect()
-                }
-                return result
-            }
-
-            override fun onPostExecute(result: String?) {
-                super.onPostExecute(result)
-                cancelProgressDialog()
-                Log.i("JSON RESPONSE RESULT", result.toString())
-            }
-            private fun showProgressDialog(){
-                customProgressDialog = Dialog(this@MainActivity)
-                customProgressDialog.setContentView(R.layout.dialog_custom_progress)
-                customProgressDialog.show()
-            }
-
-            private fun cancelProgressDialog(){
-                customProgressDialog.dismiss()
             }
 
         }
+     private fun isLocationEnabled(): Boolean{
+         val locationManager: LocationManager =
+             getSystemService(Context.LOCATION_SERVICE) as LocationManager
+         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                 || locationManager.isProviderEnabled(
+             LocationManager.NETWORK_PROVIDER
 
-
-
-
-
-
-
-
+         )
+     }
 
 
 }
